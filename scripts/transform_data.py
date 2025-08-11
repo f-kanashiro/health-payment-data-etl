90de6015b4cd0a25a1953131b366ffd4098f6ad4
Felipe Kanashiro <felipekanashiro@live.com>
2025-08-11 12:44:56 -0300

    add recipient dimension table

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -1,13 +1,46 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import (col, when, concat)
 
 spark = SparkSession.builder \
     .appName("health-payment-data-etl") \
     .getOrCreate()
 
-df = spark.read.csv(
-    "/home/fkanashiro/health-payment-data-etl/data/OP_DTL_GNRL_PGYR2019_P06302025_06162025.csv",
+df_raw_health_payment_data = spark.read.csv(
+    "/home/fkanashiro/health-payment-data-etl/data/raw/OP_DTL_GNRL_PGYR2019_P06302025_06162025.csv",
     header=True,
     inferSchema=True
 )
 
-df.show()
+#df_raw_health_payment_data.printSchema()
+
+df_dimension_recipient = df_raw_health_payment_data.select(
+    "Covered_Recipient_Profile_ID",
+    "Covered_Recipient_NPI",
+    "Covered_Recipient_First_Name",
+    "Covered_Recipient_Middle_Name",
+    "Covered_Recipient_Last_Name",
+    "Covered_Recipient_Name_Suffix",
+    "Recipient_Primary_Business_Street_Address_Line1",
+    "Recipient_Primary_Business_Street_Address_Line2",
+    "Recipient_City",
+    "Recipient_State",
+    "Recipient_Zip_Code",
+    "Recipient_Country",
+    "Recipient_Province",
+    "Recipient_Postal_Code",
+    "Covered_Recipient_Primary_Type_1",
+    "Covered_Recipient_Specialty_1",
+    "Covered_Recipient_License_State_code1"
+).dropDuplicates()
+
+df_dimension_recipient = df_dimension_recipient.withColumn(
+    "recipient_natural_key", col("Covered_Recipient_Profile_ID")
+)
+
+#df_dimension_recipient.show()
+
+df_dimension_recipient.write.mode("overwrite").parquet("/home/fkanashiro/health-payment-data-etl/data/processed")
+
+df_dimension_recipient_parquet = spark.read.parquet("/home/fkanashiro/health-payment-data-etl/data/processed")
+
+df_dimension_recipient_parquet.show()
