cd69ebfa4fdd137b6b88790a55e0c79198cb76e0
Felipe Kanashiro <felipekanashiro@live.com>
2025-08-15 00:13:59 -0300

    fix column switch when Contextual_Information have line breaks

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import (col, when, concat, row_number)
 from pyspark.sql.window import Window
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DecimalType, DateType
 
 class HealthDataProcessing:
     def __init__(self):
@@ -12,10 +13,107 @@ class HealthDataProcessing:
             .getOrCreate()
         )
 
+        data_schema = StructType([
+            StructField("Change_Type", StringType(), False),
+            StructField("Covered_Recipient_Type", StringType(), False),
+            StructField("Teaching_Hospital_CCN", IntegerType(), True),
+            StructField("Teaching_Hospital_ID", IntegerType(), True),
+            StructField("Teaching_Hospital_Name", StringType(), True),
+            StructField("Covered_Recipient_Profile_ID", IntegerType(), True),
+            StructField("Covered_Recipient_NPI", IntegerType(), True),
+            StructField("Covered_Recipient_First_Name", StringType(), True),
+            StructField("Covered_Recipient_Middle_Name", StringType(), True),
+            StructField("Covered_Recipient_Last_Name", StringType(), True),
+            StructField("Covered_Recipient_Name_Suffix", StringType(), True),
+            StructField("Recipient_Primary_Business_Street_Address_Line1", StringType(), False),
+            StructField("Recipient_Primary_Business_Street_Address_Line2", StringType(), True),
+            StructField("Recipient_City", StringType(), False),
+            StructField("Recipient_State", StringType(), True),
+            StructField("Recipient_Zip_Code", StringType(), True),
+            StructField("Recipient_Country", StringType(), False),
+            StructField("Recipient_Province", StringType(), True),
+            StructField("Recipient_Postal_Code", StringType(), True),
+            StructField("Covered_Recipient_Primary_Type_1", StringType(), True),
+            StructField("Covered_Recipient_Primary_Type_2", StringType(), True),
+            StructField("Covered_Recipient_Primary_Type_3", StringType(), True),
+            StructField("Covered_Recipient_Primary_Type_4", StringType(), True),
+            StructField("Covered_Recipient_Primary_Type_5", StringType(), True),
+            StructField("Covered_Recipient_Primary_Type_6", StringType(), True),
+            StructField("Covered_Recipient_Specialty_1", StringType(), True),
+            StructField("Covered_Recipient_Specialty_2", StringType(), True),
+            StructField("Covered_Recipient_Specialty_3", StringType(), True),
+            StructField("Covered_Recipient_Specialty_4", StringType(), True),
+            StructField("Covered_Recipient_Specialty_5", StringType(), True),
+            StructField("Covered_Recipient_Specialty_6", StringType(), True),
+            StructField("Covered_Recipient_License_State_code1", StringType(), True),
+            StructField("Covered_Recipient_License_State_code2", StringType(), True),
+            StructField("Covered_Recipient_License_State_code3", StringType(), True),
+            StructField("Covered_Recipient_License_State_code4", StringType(), True),
+            StructField("Covered_Recipient_License_State_code5", StringType(), True),
+            StructField("Submitting_Applicable_Manufacturer_or_Applicable_GPO_Name", StringType(), False),
+            StructField("Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_ID", IntegerType(), False),
+            StructField("Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_Name", StringType(), False),
+            StructField("Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_State", StringType(), True),
+            StructField("Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_Country", StringType(), False),
+            StructField("Total_Amount_of_Payment_USDollars", DecimalType(), False),
+            StructField("Date_of_Payment", DateType(), False),
+            StructField("Number_of_Payments_Included_in_Total_Amount", IntegerType(), False),
+            StructField("Form_of_Payment_or_Transfer_of_Value", StringType(), False),
+            StructField("Nature_of_Payment_or_Transfer_of_Value", StringType(), False),
+            StructField("City_of_Travel", StringType(), True),
+            StructField("State_of_Travel", StringType(), True),
+            StructField("Country_of_Travel", StringType(), True),
+            StructField("Physician_Ownership_Indicator", StringType(), True),
+            StructField("Third_Party_Payment_Recipient_Indicator", StringType(), False),
+            StructField("Name_of_Third_Party_Entity_Receiving_Payment_or_Transfer_of_Value", StringType(), True),
+            StructField("Charity_Indicator", StringType(), True),
+            StructField("Third_Party_Equals_Covered_Recipient_Indicator", StringType(), True),
+            StructField("Contextual_Information", StringType(), True),
+            StructField("Delay_in_Publication_Indicator", StringType(), False),
+            StructField("Record_ID", IntegerType(), False),
+            StructField("Dispute_Status_for_Publication", StringType(), False),
+            StructField("Related_Product_Indicator", StringType(), False),
+            StructField("Covered_or_Noncovered_Indicator_1", StringType(), True),
+            StructField("Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_1", StringType(), False),
+            StructField("Product_Category_or_Therapeutic_Area_1", StringType(), True),
+            StructField("Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_1", StringType(), True),
+            StructField("Associated_Drug_or_Biological_NDC_1", StringType(), True),
+            StructField("Associated_Device_or_Medical_Supply_PDI_1", StringType(), True),
+            StructField("Covered_or_Noncovered_Indicator_2", StringType(), True),
+            StructField("Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_2", StringType(), True),
+            StructField("Product_Category_or_Therapeutic_Area_2", StringType(), True),
+            StructField("Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_2", StringType(), True),
+            StructField("Associated_Drug_or_Biological_NDC_2", StringType(), True),
+            StructField("Associated_Device_or_Medical_Supply_PDI_2", StringType(), True),
+            StructField("Covered_or_Noncovered_Indicator_3", StringType(), True),
+            StructField("Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_3", StringType(), True),
+            StructField("Product_Category_or_Therapeutic_Area_3", StringType(), True),
+            StructField("Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_3", StringType(), True),
+            StructField("Associated_Drug_or_Biological_NDC_3", StringType(), True),
+            StructField("Associated_Device_or_Medical_Supply_PDI_3", StringType(), True),
+            StructField("Covered_or_Noncovered_Indicator_4", StringType(), True),
+            StructField("Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_4", StringType(), True),
+            StructField("Product_Category_or_Therapeutic_Area_4", StringType(), True),
+            StructField("Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_4", StringType(), True),
+            StructField("Associated_Drug_or_Biological_NDC_4", StringType(), True),
+            StructField("Associated_Device_or_Medical_Supply_PDI_4", StringType(), True),
+            StructField("Covered_or_Noncovered_Indicator_5", StringType(), True),
+            StructField("Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_5", StringType(), True),
+            StructField("Product_Category_or_Therapeutic_Area_5", StringType(), True),
+            StructField("Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_5", StringType(), True),
+            StructField("Associated_Drug_or_Biological_NDC_5", StringType(), True),
+            StructField("Associated_Device_or_Medical_Supply_PDI_5", StringType(), True),
+            StructField("Program_Year", IntegerType(), False),
+            StructField("Payment_Publication_Date", DateType(), False)
+        ])
+
         self.df_raw_health_payment_data = self.spark.read.csv(
             "/home/fkanashiro/health-payment-data-etl/data/raw/OP_DTL_GNRL_*.csv",
             header=True,
-            inferSchema=True
+            schema=data_schema,
+            quote='"',
+            escape='"',
+            multiLine=True
         )
 
     def process_manufacturer_dimension(self):
@@ -39,12 +137,6 @@ class HealthDataProcessing:
         df_manufacturer_dimension.write.mode("overwrite").parquet("/home/fkanashiro/health-payment-data-etl/data/processed/manufacturer")
 
     def process_recipient_dimension(self):
-        #df_raw_health_payment_data = self.spark.read.csv(
-        #    "/home/fkanashiro/health-payment-data-etl/data/raw/OP_DTL_GNRL_*.csv",
-        #    header=True,
-        #    inferSchema=True
-        #)
-
         recipient_id_window = Window.partitionBy("Covered_Recipient_Profile_ID").orderBy("Covered_Recipient_Profile_ID")
 
         df_recipient_dimension = (self.df_raw_health_payment_data.select(
@@ -76,7 +168,32 @@ class HealthDataProcessing:
 
         df_recipient_dimension.write.mode("overwrite").parquet("/home/fkanashiro/health-payment-data-etl/data/processed/recipient")
 
+    def process_hospital_dimension(self):
+        teaching_hospital_id_window = (
+            Window.partitionBy("Teaching_Hospital_ID")
+                .orderBy(
+                    col("Teaching_Hospital_CCN").desc_nulls_last(),
+                    col("Teaching_Hospital_Name")
+                )
+        )            
+
+        df_teaching_hospital_dimension = (self.df_raw_health_payment_data.select(
+            "Teaching_Hospital_ID",
+            "Teaching_Hospital_CCN",
+            "Teaching_Hospital_Name"
+        )
+        .withColumn("row_num", row_number().over(teaching_hospital_id_window))
+        .filter(col("row_num") == 1)
+        .drop("row_num"))
+
+        df_teaching_hospital_dimension = df_teaching_hospital_dimension.withColumn(
+            "teaching_hospital_natural_key", col("Teaching_Hospital_ID")
+        )
+
+        df_teaching_hospital_dimension.write.mode("overwrite").parquet("/home/fkanashiro/health-payment-data-etl/data/processed/teaching_hospital")        
+
 
 HDP = HealthDataProcessing()
-#HDP.process_recipient_dimension()
+HDP.process_recipient_dimension()
 HDP.process_manufacturer_dimension()
+HDP.process_hospital_dimension()
