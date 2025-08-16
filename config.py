cac06338dc9549bdc6b3915fa3a3a784d46dc2b9
Felipe Kanashiro <felipekanashiro@live.com>
2025-08-16 03:24:56 -0300

    add main, config and clean_csv_data file

---
+++ config.py
@@ -0,0 +1,6 @@
+RAW_DATA_DIR = "/home/fkanashiro/health-payment-data-etl/data/raw/"
+PROCESSED_DATA_DIR = "/home/fkanashiro/health-payment-data-etl/data/processed/"
+READY_DATA_DIR = "/home/fkanashiro/health-payment-data-etl/data/ready/"
+RAW_CSV_FILES_PATTERN = "OP_DTL_GNRL_PGYR*.csv"
+IDX_CONTEXTUAL_INFORMATION_COLUMN = 54
+PROCESS_POOL_MAX_WORKERS = 4
---
+++ main.py
@@ -0,0 +1,9 @@
+from scripts import clean_csv_data
+from scripts import transform_data
+
+def main():
+    clean_csv_data.run()
+    #transform_data.run()
+
+if __name__ == "__main__":
+    main()
