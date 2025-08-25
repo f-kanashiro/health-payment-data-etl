9fd2346ac1ae89ac532a4d86ecc625f2026aaf55
Felipe Kanashiro <felipekanashiro@live.com>
2025-08-25 00:53:02 -0300

    add minio buckets for raw and processed data

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from scripts import transform_data
 from scripts import load_to_db
 
 def main():
-    #split_raw_csv.run()
+    split_raw_csv.run()
     #transform_data.run()
-    load_to_db.run()
+    #load_to_db.run()
 
 if __name__ == "__main__":
     main()
--- scripts/split_raw_csv.py
+++ scripts/split_raw_csv.py
@@ -2,28 +2,35 @@ import os
 import glob
 import pandas as pd
 import boto3
+import fnmatch
 from multiprocessing import Pool, cpu_count
-from io import StringIO
+from io import StringIO, TextIOWrapper
 
 import config
 
-minio_client = boto3.client(
-    "s3",
-    endpoint_url=config.MINIO_ENDPOINT,
-    aws_access_key_id=config.MINIO_ACCESS_KEY,
-    aws_secret_access_key=config.MINIO_SECRET_KEY
-)
+
 
 def split_csv(csv):
+    minio_client = boto3.client(
+        "s3",
+        endpoint_url=config.MINIO_ENDPOINT,
+        aws_access_key_id=config.MINIO_ACCESS_KEY,
+        aws_secret_access_key=config.MINIO_SECRET_KEY
+    )
+
     base_name = os.path.splitext(os.path.basename(csv))[0]
 
+    raw_csv = minio_client.get_object(Bucket=config.MINIO_BUCKET_RAW_DATA, Key=csv)
+    file_body = raw_csv["Body"]
+
+    text_stream = TextIOWrapper(file_body, encoding="utf-8")
+
     csv_reader = pd.read_csv(
-        csv,
+        text_stream,
         chunksize=config.ROWS_PER_CSV_SPLIT,
         quotechar='"',
         escapechar='\\',
         engine='python',
-        encoding='utf-8',
         dtype=str
     )
 
@@ -31,7 +38,7 @@ def split_csv(csv):
         out_key = f"{base_name}_part_{i}.csv"
         csv_buffer = StringIO()
         chunk.to_csv(csv_buffer, index=False)
-        csv_buffer.seek(0)
+        #csv_buffer.seek(0)
 
         minio_client.put_object(
             Bucket=config.MINIO_BUCKET_PROCESSED_DATA,
@@ -41,10 +48,27 @@ def split_csv(csv):
 
 
 def parallel_process_raw_csv():
-    csv_files = glob.glob(os.path.join(config.RAW_DATA_DIR, config.RAW_CSV_FILES_PATTERN))
+    #csv_files = glob.glob(os.path.join(config.RAW_DATA_DIR, config.RAW_CSV_FILES_PATTERN))
+    #if not csv_files:
+        #return
+
+    minio_client = boto3.client(
+        "s3",
+        endpoint_url=config.MINIO_ENDPOINT,
+        aws_access_key_id=config.MINIO_ACCESS_KEY,
+        aws_secret_access_key=config.MINIO_SECRET_KEY
+    )
+    raw_files = minio_client.list_objects_v2(Bucket=config.MINIO_BUCKET_RAW_DATA)
+    csv_files = [
+        obj["Key"]
+        for obj in raw_files.get("Contents", [])
+        if fnmatch.fnmatch(obj["Key"], config.RAW_CSV_FILES_PATTERN)
+    ]
+
     if not csv_files:
         return
 
+    print("Test")
     with Pool(processes=cpu_count()) as pool:
         pool.map(split_csv, csv_files)
 
